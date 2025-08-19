d933f82f4deed58fe79f168b833884067a7ff1f8
gudluririshika724-ux <gudluririshika724@gmail.com>
2025-08-19 11:24:50 +0530

    19/08/2025

--- week1p.py
+++ week1p.py
@@ -2,43 +2,55 @@ class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
-class LinkedList(self):
-    self.head=None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
 
 
 # traversing the linkedList
     def traverse(self):
         current=self.head
         while current:
-            print(current.val,"-->")
+            print(current.val,"-->",end=" ")
             current=current.next
         print("none")
+
+
 # insert element at the start 
-    def addAtstart(self):
+    def addAtstart(self,val):
         newnode=ListNode(val)
         newnode.next=self.head
         self.head=newnode
+
+
 # insert element at the end 
-    def addatend(self):
+    def addatend(self, val):
         newnode=ListNode(val)
         if not self.head:
             self.head=newnode
             return
         current=self.head
+
         while current.next:
             current=current.next
         current.next=newnode
+
 # insert at position
-    def addatpostion(self,pos):
+    def addatpostion(self,pos,val):
         newnode=ListNode(val)
-        if(pos==1):
+        if pos==1:
             newnode.next=self.head
             self.head=newnode
+            return
+        
         current =self.head
         count =1
         while current is not None and count<pos-1:
             current=current.next
             count =count+1
+
         if current is None:
             print("Position out of range")
             return
@@ -46,5 +58,21 @@ class LinkedList(self):
         # Insert the new node
         newnode.next = current.next
         current.next = newnode
+
+    def display(self):
+        temp=self.head
+        while(temp!=None):
+            print(str(temp.val)+"-->",end=" ")
+            temp=temp.next
+        print("END")
+
+
+ll=LinkedList()
+ll.addAtstart(5)
+ll.addatend(10)
+ll.addatend(15)
+ll.addatpostion(1,50)
+ll.display()
+
 
 
