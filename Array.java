8a1f95c482b524e23b50f66ecef435e84d00bce6
gudluririshika724-ux <gudluririshika724@gmail.com>
2025-08-20 14:25:17 +0530

    20/08/2025

---
+++ Array.java
@@ -0,0 +1,61 @@
+// Reverse a sub array of an given array of length k
+import java.util.ArrayList;
+public class Array {
+
+    static void reverseInGroups(int[] arr, int k){
+        int n = arr.length; 
+
+        for (int i = 0; i < n; i += k) {
+            int left = i;
+            int right = Math.min(i + k - 1, n - 1); 
+
+            // reverse the sub-array
+            while (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    
+
+
+    public static ArrayList<Integer> findDuplicates(int arr[]){
+        ArrayList<Integer> ans =new ArrayList<>();
+        for(int i=0;i< arr.length;i++){
+            int c=0;
+            for(int it :ans){
+                if(arr[i]==it){
+                    c++;
+                    break;
+                }
+
+            }
+            if(c>0){continue;}
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]){
+                    c++;
+                    break;
+                }
+            }
+
+            // if duplicate found, add to result
+            if (c > 0) ans.add(arr[i]);
+        }
+
+        return ans;
+        
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int k = 3;
+
+        reverseInGroups(arr, k);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
